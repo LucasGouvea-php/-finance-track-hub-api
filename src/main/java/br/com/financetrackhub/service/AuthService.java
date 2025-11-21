292c021927b2824d7c013fc5ea2641206e920748
LucasGouvea <lucasgouveaopt@gmail.com>
2025-11-21 11:59:22 -0300

    correções finais

--- src/main/java/br/com/financetrackhub/service/AuthService.java
+++ src/main/java/br/com/financetrackhub/service/AuthService.java
@@ -8,7 +8,6 @@ import br.com.financetrackhub.exception.BadRequestException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
--- src/test/java/br/com/financetrackhub/FinanceTrackHubApiApplicationTests.java
+++ src/test/java/br/com/financetrackhub/FinanceTrackHubApiApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.financetrackhub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class FinanceTrackHubApiApplicationTests {
 
 	@Test
